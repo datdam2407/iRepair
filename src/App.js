5aba57d384694f97542a1aef7c7667be67cef0a4
Dat <datdam2407@gmail.com>
2021-09-10 19:50:32 +0700

    change commit from irepair_reactDev

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import "./App.css";
-
-import Header from "./components/Header/headerPage";
-// import Menu from "./components/Menu";
-
-import { Row, Col } from "reactstrap";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from "./components/Login/Login";
-import Admin from "./components/MainContent/MainContent";
-// import Login2 from "./components/Login/Login";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <Router>
-        {/* <Header /> */}
-        <Row className="fixed">
-          <Col>
-            <Switch>
-              <Route path="/" exact component={Login} />
-              <Route path="/Admin" component={Admin} />
-              {/* <Route path="/Admin" component={use} /> */}
-            </Switch>
-          </Col>
-        </Row>
-      </Router>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import React from "react";
 import ReactDOM from "react-dom";
@@ -31,3 +32,22 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
+=======
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+>>>>>>> 4f13f12... Initialize project using Create React App
