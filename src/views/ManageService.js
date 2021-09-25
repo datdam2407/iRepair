f7851b5118bc06591aa4b2ef971d764bd7ffc4b7
Nguyễn Lê Thuần <80556049+Thuan1112@users.noreply.github.com>
2021-09-25 13:29:32 +0700

    change commit

--- src/views/ManageService.js
+++ src/views/ManageService.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import data from "./mock_data.json";
-import ReadOnlyRows from "../components/ReadOnlyRows";
 
 // react-bootstrap components
 import {
